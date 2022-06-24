20c0f68d64f86fc0151d640a007b6af1d1e4f256
Lukeluds <luyuan925@163.com>
2022-06-24 11:14:38 -0500

    adding Genres page layout

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import Splash from "./Splash/Splash";
 import About from "./About/About";
 import GameList from "./GamePages/GameList";
 import CompanyList from "./CompanyPages/CompanyList";
+import Genres from "./Genres/Genres"
 import {
   BrowserRouter as Router,
   Route,
@@ -40,13 +41,13 @@ export default function AppPages() {
     <Router>
       <div>
         <Routes>
-          <Route path='/' element={<Splash/>} />
-          <Route path='/about' element={<About/>} />
-          <Route path='/games' element={<GameList/>} />
-          <Route path='/companies' element={<CompanyList/>} />
+          <Route path='/' element={<Splash />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/games' element={<GameList />} />
+          <Route path='/companies' element={<CompanyList />} />
+          <Route path='/genres' element={<Genres />} />
         </Routes>
       </div>
     </Router>
   );
 }
-
---
+++ src/Genres/Genres.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react'
+import "../StyleAndImg/style.css";
+import logo from "../StyleAndImg/logosmall.png"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+const GameList = (props) => {
+
+    return (
+        <div className='page'>
+            <link rel="stylesheet" href="style.css" type="text/css" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap"
+                rel="stylesheet"
+            />
+
+            <nav class="navbar navbar-expand-lg navbar-dark">
+                <img src={logo} alt="logo" style={{ width: 70, marginLeft: 10 }} />
+                <a class="navbar-brand" href="#">GameHub</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <Link to="/">
+                            <a class="nav-item nav-link">Home</a>
+                        </Link>
+                        <Link to="/games">
+                            <a class="nav-item nav-link">Games</a>
+                        </Link>
+                        <Link to="/companies">
+                            <a class="nav-item nav-link">Companies</a>
+                        </Link>
+                        <Link to="/genres">
+                            <a class="nav-item nav-link active">Genres</a>
+                        </Link>
+                        <Link to="/about">
+                            <a class="nav-item nav-link">About Us</a>
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+            <div className="listTitleText" style={{ animation: "fadeIn 0.5s" }}>
+                Genres
+            </div>
+
+
+            <Link to="/">
+                <div className="animated-button">
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    Back
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default GameList
