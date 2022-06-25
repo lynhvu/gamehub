13a54867c2636d5f7c46d92dc19c7fbdec02e5e1
Lauren <lblummer@utexas.edu>
2022-06-25 09:43:30 -0500

    fixed navbar active status #17

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,8 +5,7 @@ import {
     Switch,
     Route,
     Link,
-    useMatch,
-    useResolvedPath
+    NavLink
   } from "react-router-dom";
 
 const NavBar = () => {
@@ -14,27 +13,27 @@ const NavBar = () => {
         
         <nav class="navbar navbar-expand-lg navbar-dark">
             <img src={logo} alt="logo" style={{width: 70, marginLeft:10}}/>
-            <a class="navbar-brand" href="#">GameHub</a>
+            <a class="navbar-brand" href="/">GameHub</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <Link to="/" className="nav-item nav-link">
+                    <NavLink to="/" activeClassName="navbar__link--active" className="navbar__link nav-item nav-link">
                         Home
-                    </Link>
-                    <Link to="/games" className="nav-item nav-link">
+                    </NavLink>
+                    <NavLink to="/games" activeClassName="navbar__link--active" className="navbar__link nav-item nav-link">
                         Games
-                    </Link>
-                    <Link to="/companies" className="nav-item nav-link">
+                    </NavLink>
+                    <NavLink to="/companies" activeClassName="navbar__link--active" className="navbar__link nav-item nav-link">
                         Companies
-                    </Link>
-                    <Link to="/genres" className="nav-item nav-link">
+                    </NavLink>
+                    <NavLink to="/genres" activeClassName="navbar__link--active" className="navbar__link nav-item nav-link">
                         Genres
-                    </Link>
-                    <Link to="/about" className="nav-item nav-link">
+                    </NavLink>
+                    <NavLink to="/about" activeClassName="navbar__link--active" className="navbar__link nav-item nav-link">
                         About Us
-                    </Link>
+                    </NavLink>
                 </div>
             </div>
         </nav>
