0f540707b06c84d45ba79977c47c6dc487f439ee
Linh Vu <linhvu.138@utexas.edu>
2022-06-26 15:41:24 -0400

    about page - added group stats + links

---
+++ src/AboutUs/GroupStats.js
@@ -0,0 +1,40 @@
+import "../StyleAndImg/style.css";
+
+const GroupStats = (props) => {
+    var stats = require('./totalStats.json');
+    return (   
+        <div className="container-fluid">
+            <div className="row card-solid" id="total-stats" style={{margin: "5%"}}>
+                <h1>Group Stats</h1>    
+                    {stats.map(item =>
+                    <div className='col card-inside'>
+                        <p>{item.type}</p>
+                        <h1>{item.number}</h1>
+                        
+                    </div>
+                )}
+            </div>
+            <div className="row card-solid" id="total-stats" style={{margin: "5%"}}> 
+
+                <div className="col card-inside d-inline-flex align-items-center justify-content-center">
+                    <a className="link-style">Postman</a>
+                </div> 
+
+                <div className="col card-inside d-inline-flex align-items-center justify-content-center">
+                    <a href="https://gitlab.com/lynhvu138/cs373-idb/-/issues" className="link-style">GitLab Issue Tracker</a>
+                </div> 
+
+                <div className="col card-inside d-inline-flex align-items-center justify-content-center">
+                    <a href="https://gitlab.com/lynhvu138/cs373-idb" className="link-style">Gitlab Repo</a>
+                </div> 
+
+                <div className="col card-inside d-inline-flex align-items-center justify-content-center">
+                    <a href="https://gitlab.com/lynhvu138/cs373-idb/-/wikis/home" className="link-style">GitLab Wiki</a>
+                </div> 
+
+            </div>
+        </div>
+    )
+}
+
+export default GroupStats
