61826c777fc12e6cec98f665b6f2dc3ebb234157
Lauren <lblummer@utexas.edu>
2022-06-26 14:13:34 -0500

    added id numbers to company data in base.py

--- backend/base.py
+++ backend/base.py
@@ -3,6 +3,7 @@ import json
 
 companyData = [
     {
+        "id": "0",
         "name": "Naughty Dog",
         "description": "Established in 1984, Naughty Dog is one of the most successful and prolific game development studios in the world and a flagship first-party studio within PlayStation Studios. From creating the iconic Crash Bandicoot and Jak and Daxter series to modern franchises like Uncharted and The Last of Us, Naughty Dog is responsible for some of the most critically acclaimed and commercially successful games on Sony’s PlayStation platforms. Through its use of cutting-edge technology and evocative, character-driven storytelling, Naughty Dog has received hundreds of industry and media awards, while developing a passionate fan base of millions of players around the globe.",
         "location": "United States",
@@ -22,6 +23,7 @@ companyData = [
         "img": "https://images.igdb.com/igdb/image/upload/t_logo_med/bcpeptiqicy9g0gn4nur.png",
     },
     {
+        "id": "1",
         "name": "Obsidian Entertainment",
         "description": "Obsidian Entertainment is an American video game developer based in Irvine, California. Former employees of Black Isle Studios founded it. The studio is well-known for their critically acclaimed RPGs such as Pillars of Eternity series, Star Wars Knights of the Old Republic II: The Sith Lords and Fallout New Vegas. Obsidian’s projects often use well-established franchises as a setting. The first two projects developed by the studio were sequels to Star Wars: Knights of the Old Republic and Neverwinter Nights. In 2014 Ubisoft released South Park: The Stick of Truth developed by Obsidian. The game was an RPG based on South Park franchise. It was a commercial success with more than 5 million copies sold worldwide in two years and was well received by critics, players, and fans of the show alike. The developer also created several games using their original IPs: Alpha Protocol, Pillars of Eternity series and Tyranny all of which were either RPGs or included role-playing elements in gameplay.",
         "location": "United States",
@@ -41,6 +43,7 @@ companyData = [
         "img": "https://images.igdb.com/igdb/image/upload/t_logo_med/cl1my.png",
     },
     {
+        "id": "2",
         "name": "Valve Corporation",
         "description": "Valve's debut title, Half-Life, was released in 1998. It won more than 50 game of the year awards, and PC Gamer even called it the Best PC Game Ever. Since then, we've released dozens of titles that changed the world. Today, millions of people play our games every day.",
         "location": "United States",
