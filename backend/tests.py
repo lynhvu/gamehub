284f585da021d0ee602217e00ed6ab206b004f30
Lukeluds <luyuan925@163.com>
2022-07-07 20:43:30 -0500

    fixing tests bugs hopefully

--- backend/tests.py
+++ backend/tests.py
@@ -8,7 +8,7 @@ from models import db, Company, Game, Genre
 import requests
 
 
-class TestModels (TestCase):
+class TestModels(TestCase):
     # ----
     # read
     # ----
@@ -22,20 +22,14 @@ class TestModels (TestCase):
     def test_company1(self):
         temp = db.session.query(Company).filter_by(id=2).one()
         self.assertEqual(temp.id, 2)
-        self.assertEqual(str(temp.name), "Valve Corporation")
-        self.assertEqual(
-            str(temp.description),
-            "Valve's debut title, Half-Life, was released in 1998. It won more than 50 game of the year awards, and PC Gamer even called it the Best PC Game Ever. Since then, we've released dozens of titles that changed the world. Today, millions of people play our games every day.",
-        )
+        self.assertEqual(str(temp.name), "Valve Software")
         self.assertEqual(str(temp.location), "United States")
-        self.assertEqual(str(temp.year), "1996")
-        # self.assertEqual(temp.num_games, 0)      # Could not find this info from models.py
-        # TODO: not sure how to check for relationship
+        self.assertEqual(temp.num_games, 42)
         self.assertEqual(
             str(temp.img),
-            "https://images.igdb.com/igdb/image/upload/t_logo_med/cl2he.png",
+            "https://media.rawg.io/media/games/46d/46d98e6910fbc0706e2948a7cc9b10c5.jpg",
         )
-    
+
     def test_company2(self):
         temp = db.session.query(Company).filter_by(id=0).one()
         self.assertEqual(temp.id, 0)
@@ -46,8 +40,6 @@ class TestModels (TestCase):
         )
         self.assertEqual(str(temp.location), "United States")
         self.assertEqual(str(temp.year), "1984")
-        # self.assertEqual(temp.num_games, 0)      # Could not find this info from models.py
-        # TODO: not sure how to check for relationship
         self.assertEqual(
             str(temp.img),
             "https://images.igdb.com/igdb/image/upload/t_logo_med/bcpeptiqicy9g0gn4nur.png",
@@ -56,16 +48,9 @@ class TestModels (TestCase):
     def test_game1(self):
         temp = db.session.query(Game).filter_by(id=1).one()
         self.assertEqual(temp.id, 1)
-        self.assertEqual(str(temp.name), "The Last of Us")
-        self.assertEqual(
-            str(temp.description),
-            "The population of the Earth almost disappeared as a result of a pandemic caused by a mutated fungus. The disease causes irreversible changes in the human body, a person loses his mind and behaves aggressively, like a zombie. Civilization no longer exists, few survivors live in isolation under the protection of the military. Cities outside the zones are dangerous ruins inhabited by infected people and people who have almost lost humanity.",
-        )
-        self.assertEqual(temp.score, 95)
-        self.assertEqual(str(temp.released), "Jun 14, 2013")
-        # self.assertEqual(temp.genre_id, )
-        # self.assertEqual(temp.company_id, )
-        # TODO: not sure how to check for genre_id, comp_id, pictures and platforms
+        self.assertEqual(str(temp.name), "The Witcher 3: Wild Hunt")
+        self.assertEqual(str(temp.released), "2015-05-18")
+        self.assertEqual(temp.score, 92)
 
     def test_game2(self):
         temp = db.session.query(Game).filter_by(id=0).one()
@@ -82,16 +67,11 @@ class TestModels (TestCase):
         temp = db.session.query(Genre).filter_by(id=0).one()
         self.assertEqual(temp.id, 0)
         self.assertEqual(str(temp.name), "Action")
-        self.assertEqual(
-            str(temp.description),
-            "An action game is a video game genre that emphasizes physical challenges, including hand–eye coordination and reaction-time. The genre includes a large variety of sub-genres, such as fighting games, beat 'em ups, shooter games, and platform games. Multiplayer online battle arena and some real-time strategy games are also considered action games.",
-        )
-        self.assertEqual(temp.num_games, 153616)
+        self.assertEqual(temp.num_games, 154515)
         self.assertEqual(
             str(temp.themes),
-            "physical combats, quick reflexes, abilities and specials, obstables and enemies",
+            "Physical combat, Quick reflexes, Abilities, Obstables and Enemies",
         )
-        # TODO: not sure how to check for relationship
 
     def test_genre2(self):
         temp = db.session.query(Genre).filter_by(id=1).one()
@@ -107,36 +87,41 @@ class TestModels (TestCase):
             "exploration, gathering and looting, stories and settings",
         )
 
-
-# ----
-# main
-# ----
+    # ----
+    # main
+    # ----
 
     # these three tests are to make sure our apis running
     def test_comp_api(self):
         endpoint = "http://gamehubapi.me/companies/"
         r = requests.get(endpoint)
         self.assertEqual(200, r.status_code)
+
     def test_genres_api(self):
         endpoint = "http://gamehubapi.me/genres/"
         r = requests.get(endpoint)
         self.assertEqual(200, r.status_code)
+
     def test_games_api(self):
         endpoint = "http://gamehubapi.me/games/"
         r = requests.get(endpoint)
         self.assertEqual(200, r.status_code)
+
     def test_comp_api_1(self):
         endpoint = "http://gamehubapi.me/companies/id:1"
         r = requests.get(endpoint)
         self.assertEqual(200, r.status_code)
+
     def test_genres_api_1(self):
         endpoint = "http://gamehubapi.me/genres/id:1"
         r = requests.get(endpoint)
         self.assertEqual(200, r.status_code)
+
     def test_games_api_1(self):
         endpoint = "http://gamehubapi.me/games/id:1"
         r = requests.get(endpoint)
         self.assertEqual(200, r.status_code)
+
 
 if __name__ == "__main__":
     main()
