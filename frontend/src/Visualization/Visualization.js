44ab081c8e0af4c5f6f11397b6fad603733bf7f8
Lauren <lblummer@utexas.edu>
2022-07-10 18:38:29 -0500

    code for pulling data from group1 API added

--- frontend/src/Visualization/Visualization.js
+++ frontend/src/Visualization/Visualization.js
@@ -1,9 +1,42 @@
 import NavBar from '../components/NavBar';
+import { useState, useEffect } from "react";
 
 //https://observablehq.com/@d3/gallery - gallery of possible d3 visualizations
 
 const Visualization = (props) => {
-    return (
+
+  const [songs, setSongs] = useState([])
+  const [albums, setAlbums] = useState([])
+  const [artists, setArtists] = useState([])
+
+  useEffect(() => {
+    fetch("https://cs373-idb-backend.uc.r.appspot.com/songs/")
+      .then((res) => res.json())
+      .then((data) => {
+        setSongs(data);
+        console.log(data);
+        console.log(data.length);
+      })
+      .catch((err) => console.log(err));
+    fetch("https://cs373-idb-backend.uc.r.appspot.com/albums/")
+      .then((res) => res.json())
+      .then((data) => {
+        setAlbums(data);
+        console.log(data);
+        console.log(data.length);
+      })
+      .catch((err) => console.log(err));
+    fetch("https://cs373-idb-backend.uc.r.appspot.com/artists/")
+      .then((res) => res.json())
+      .then((data) => {
+        setArtists(data);
+        console.log(data);
+        console.log(data.length);
+      })
+      .catch((err) => console.log(err));
+}, [])
+  
+  return (
         <div className="page">
           <link rel="stylesheet" href="style.css" type="text/css" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -20,8 +53,11 @@ const Visualization = (props) => {
           <br></br>
           <div className="container">
             <p style={{color: "white"}}>Visualizations go here!</p>
+            
           </div>
         </div>)
 };
+
+// we willl need titles for each one and maybe a small description of what the chart/diagram is displaying
 
 export default Visualization;
