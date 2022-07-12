76823c5356e0f4b21e1b367849a6adad7bbb2c75
Lauren <lblummer@utexas.edu>
2022-07-12 14:45:41 -0500

    getting the sets for the data correct - visualizations

--- frontend/src/Visualization/Visualization.js
+++ frontend/src/Visualization/Visualization.js
@@ -2,6 +2,7 @@ import NavBar from '../components/NavBar';
 import { useState, useEffect } from "react";
 
 //https://observablehq.com/@d3/gallery - gallery of possible d3 visualizations
+//https://recharts.org/en-US/examples
 
 const Visualization = (props) => {
 
@@ -10,28 +11,31 @@ const Visualization = (props) => {
   const [comps, setComps] = useState([])
 
   useEffect(() => {
-    fetch("https://idb-3-354621.uc.r.appspot.com/jobs")
+//    fetch("https://idb-3-354621.uc.r.appspot.com/jobs")
+    fetch("http://localhost:5000/jobs/")
       .then((res) => res.json())
       .then((data) => {
-        setJobs(data);
-        console.log(data);
-        console.log(data.length);
+        setJobs(data["Jobs"]);
+        console.log(data["Jobs"]);
+        console.log(data["Jobs"].length);
       })
       .catch((err) => console.log(err));
-    fetch("https://idb-3-354621.uc.r.appspot.com/locations")
+//    fetch("https://idb-3-354621.uc.r.appspot.com/locations")
+    fetch("http://localhost:5000/locs/")
       .then((res) => res.json())
       .then((data) => {
-        setLocs(data);
-        console.log(data);
-        console.log(data.length);
+        setLocs(data["Locations"]);
+        console.log(data["Locations"]);
+        console.log(data["Locations"].length);
       })
       .catch((err) => console.log(err));
-    fetch("https://idb-3-354621.uc.r.appspot.com/companies")
+//    fetch("https://idb-3-354621.uc.r.appspot.com/companies")
+    fetch("http://localhost:5000/comps/")
       .then((res) => res.json())
       .then((data) => {
-        setComps(data);
-        console.log(data);
-        console.log(data.length);
+        setComps(data["Companies"]);
+        console.log(data["Companies"]);
+        console.log(data["Companies"].length);
       })
       .catch((err) => console.log(err));
 }, [])
